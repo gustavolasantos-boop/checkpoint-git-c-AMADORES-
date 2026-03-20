9041b6f5d86c35bee94a288137c662a33d1abb84
gabrielfifa777-cmyk <GabrielFIFA777@gmail.com>
2026-03-20 09:44:53 -0300

    processo final

--- main.c/main.c
+++ main.c/main.c
@@ -5,7 +5,10 @@ int main(){
     printf("Gabriel Camarosani Gouvea Gonçalves da Silva");
     printf("08/07/2007");
     printf("\n");
-    printf("Guilherme Cedro");
-    printf("sextou");
+    printf("Guilherme Cedro");  
+    printf("sextou");  
+    printf("\n");
+    printf("Matheus Pimenta Martini");
+    printf("Amanhã não tem aula professor!");
     return 0;
 }
